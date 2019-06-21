cfedba2c4cf00d180f2ea992bbe34fb5d9cba622
Riche0826 <34505961+Riche0826@users.noreply.github.com>
2019-06-21 23:39:27 +0800

    Update MainActivity.java

--- app/src/main/java/flag/com/tw/ch04_ezcounter/MainActivity.java
+++ app/src/main/java/flag/com/tw/ch04_ezcounter/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
     TextView txv;
     Button addBtn;
     int counter = 1, i;
@@ -19,14 +19,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txv = (TextView) findViewById(R.id.txv);
         addBtn = (Button) findViewById(R.id.addBtn);
+
         addBtn.setOnClickListener(this);
+        addBtn.setOnLongClickListener(this);
+        txv.setOnLongClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        counter += counter;
+    public void onClick(View v) {
+        counter++;
 
         txv.setText(String.valueOf(counter));
-        addBtn.setText("加" + counter);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if(v.getId() == R.id.txv){
+            counter = 0;
+            txv.setText("0");
+        }else{
+            counter += 2;
+            txv.setText(String.valueOf(counter));
+        }
+
+        return true;
     }
 }
